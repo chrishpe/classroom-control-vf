ace1162a29120f1612348e50853f3c100df1502c
chrishpe <chris.hurley@hpe.com>
2016-02-24 09:27:02 +0000

    Refactor variables

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,25 +1,25 @@
 class nginx{
+$package = 'nginx'
 
-  package{ 'nginx':
+File{
+  owner => '0',
+  group => '0',
+  mode  => '0644',
+}
+
+  package{ $package:
     ensure  => present,
   }
   
   file { '/etc/nginx/nginx.conf':
-    ensure  => 'file',
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    #content => '{md5}060207123b3c5c5189cbe5259e1d5c2f',
-    #ctime   => '2016-02-23 13:26:16 +0000',
-    group   => '0',
-    mode    => '0644',
-    #mtime   => '2016-01-26 18:15:47 +0000',
-    owner   => '0',
-    #type    => 'file',
+    ensure    => 'file',
+    source    => 'puppet:///modules/nginx/nginx.conf',
     require   => Package['nginx'],
   }
 
   file { '/etc/nginx/conf.d/default.conf':
-    ensure => 'file',
-    source=> 'puppet:///modules/nginx/default.conf',
+    ensure  => 'file',
+    source  => 'puppet:///modules/nginx/default.conf',
     require => Package['nginx'],
   }
   
@@ -32,10 +32,10 @@ class nginx{
     source  => 'puppet:///modules/nginx/index.html',
   }
   
-  service{ 'nginx':
-    ensure  => 'running',
-    enable => 'false',
-    require => [
+  service{ $package :
+    ensure      => 'running',
+    enable      => 'false',
+    subscribe   => [
       File['/etc/nginx/conf.d/default.conf'],
       File['/etc/nginx/nginx.conf']
     ]
