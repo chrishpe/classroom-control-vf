0d5d0a18b611a8a5a602d3a27f758c3a2e4e35f1
chrishpe <chris.hurley@hpe.com>
2016-02-25 11:03:54 +0000

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -3,7 +3,7 @@ class params{
 $package_name     = 'nginx'
 $owner            = 'root'
 $group            = $owner
-$default_doc_root = '/var/www'
+$doc_root = '/var/www'
 $conf_dir         = '/etc/nginx'
 $block_dir        = "${conf_dir}/conf.d"
 $logdir           = "/var/log/${package_name}"
